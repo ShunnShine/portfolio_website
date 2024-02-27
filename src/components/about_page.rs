b9a0a9f8b68c5e4a800a6987d3dfa74a112cda9a
Jack Shunn <jack.shunn@gmail.com>
2024-02-26 17:05:21 -0700

    adds Super basic about page

--- src/components/about_page.rs
+++ src/components/about_page.rs
@@ -3,6 +3,10 @@ use leptos::*;
 #[component]
 pub fn AboutPage() -> impl IntoView {
     view! {
-        <p>"About"</p>
+        <div class="flex flex-col gap-5 p-10">
+            <h1>"Hi my name is Jack Shunn!"</h1>
+            <h2>"I am a software developer and a computer science student at the University of Utah!"</h2>
+            <h3>"This website is built using the Rust programming language using the Leptos framework. Also tailwind for styling."</h3>
+        </div>
     }
 }
